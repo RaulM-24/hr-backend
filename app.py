f5b536d40d53f0c79b2c64444193a48ea214d187
RaulM-24 <emiliomendezmendez917@gmail.com>
2026-01-16 04:16:30 -0600

    Fix mixed content HTTPS

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def update_message():
 
 # ---------- ARRANQUE ----------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
