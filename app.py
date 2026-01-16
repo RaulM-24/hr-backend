fb84696586c1638e58e09998b524abf33109684b
RaulM-24 <emiliomendezmendez917@gmail.com>
2026-01-16 04:31:20 -0600

    Fix HTTPS proxy headers for Render

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ import json
 import os
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_for=1,
+    x_proto=1,
+    x_host=1,
+    x_port=1,
+    x_prefix=1
+)
+
 CORS(app)
 
 DATA_FILE = os.path.join("data", "message.json")
